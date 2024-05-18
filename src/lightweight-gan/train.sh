eb2f1f3b6f9b04f566dc25e5def915f60ed5173b
prerana <prerana@hellosivi.com>
2024-05-18 22:56:42 +0530

    training script

--- src/lightweight-gan/train.sh
+++ src/lightweight-gan/train.sh
@@ -1,1 +1,7 @@
-accelerate launch --config accelerate_configs/accelerate_single_gpu_yaml run_train.py --aug_prob 0.6
+wandb offline
+accelerate launch --config_file accelerate_configs/accelerate_cpu.yaml run_train.py \
+--data "julianmoraes/doodles-captions-BLIP" \
+--aug_prob 0.6 \
+--sample 120 \
+--dual_contrast_loss \
+--disc_output_size 1
