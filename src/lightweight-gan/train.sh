5d11a023f7403468420e9409fd17516472078263
prerana <prerana@hellosivi.com>
2024-05-19 08:26:17 +0530

    updated training script

--- src/lightweight-gan/train.sh
+++ src/lightweight-gan/train.sh
@@ -2,6 +2,9 @@ wandb offline
 accelerate launch --config_file accelerate_configs/accelerate_cpu.yaml run_train.py \
 --data "julianmoraes/doodles-captions-BLIP" \
 --aug_prob 0.6 \
---sample 120 \
+--sample 20 \
 --dual_contrast_loss \
---disc_output_size 1
+--disc_output_size 1 \
+--calculate_fid_num_images 2 \
+--val_batch_size 1 \
+--evaluate_every 1
